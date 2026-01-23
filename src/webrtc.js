f145110314d4a973677b3f0359c665a078ed883f
Ben Carr <benbean303@gmail.com>
2026-01-24 08:21:49 +1100

    Update date

--- src/webrtc.js
+++ src/webrtc.js
@@ -4,7 +4,7 @@
 // Handles connection and data from Google WebRTC systems
 // Currently a "work in progress"
 //
-// Code version 2025.11.22
+// Code version 2026.01.24
 // Mark Hulskamp
 'use strict';
 
